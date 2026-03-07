e5957b3fa595283d490486de0b4bf671d9e552d4
Arunjenson-SurveySparrow <arun.jenson@surveysparrow.com>
2026-03-07 17:20:18 +0530

    Add initial React application structure with App.js, index.css, and index.html

---
+++ App.js
@@ -0,0 +1,25 @@
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "hello world from react");
+
+const parent = React.createElement("div", {
+    id: "parent"
+},
+    React.createElement(
+        "div", {
+        id: "child"
+    },
+        [React.createElement("h1", {
+            id: "heading2"
+        },
+            "Helllo im heading 2"),
+        React.createElement("h2", {
+            id: "heading2"
+        },
+            "Helllo im heading 3")
+        ]
+    ))
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent)
