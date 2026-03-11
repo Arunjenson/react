e08e9aa4a5815c6e02dc9bdd199ffbcbb33c5fa7
Arunjenson <jensonarun8@gmail.com>
2026-03-11 22:18:57 +0530

    Refactor App.js to use JSX for rendering elements; update fundamentals.md to clarify React component types

--- App.js
+++ App.js
@@ -1,27 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {
-    id: "heading"
-}, "hello world from react");
+// const heading = React.createElement("h1", {
+//     id: "heading"
+// }, "hello world from react");
 
-const parent = React.createElement("div", {
-    id: "parent"
-},
-    React.createElement(
-        "div", {
-        id: "child"
-    },
-        [React.createElement("h1", {
-            id: "heading2"
-        },
-            "Helllo im heading 2"),
-        React.createElement("h2", {
-            id: "heading2"
-        },
-            "Helllo im heading 3")
-        ]
-    ))
+// const parent = React.createElement("div", {
+//     id: "parent"
+// },
+//     React.createElement(
+//         "div", {
+//         id: "child"
+//     },
+//         [React.createElement("h1", {
+//             id: "heading2"
+//         },
+//             "Helllo im heading 2"),
+//         React.createElement("h2", {
+//             id: "heading2"
+//         },
+//             "Helllo im heading 3")
+//         ]
+//     ))
+
+// react element with jsx
+
+const heading = <h1 id="heading">hello world from react</h1>
+
+// react function component
+
+function HeadingComponent() {
+    return <h1 id="heading">hello world from react</h1>
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
--- notes/fundamentals.md
+++ notes/fundamentals.md
@@ -23,3 +23,10 @@ JS engine cannot understand the JSX
 JSX is transpiled before it reaches to JS  - Babel
 
 JSX => React.createElement => ReactElement JS object => HTML
+
+
+# React Component 
+
+two ways we can write react components
+1. Functional Components. => Just a normal JS function which returns a JSX
+2. Class Based Components
