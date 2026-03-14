cf2a6e78c76b7328e83cbb32fbe3f3076571347d
Arunjenson <jensonarun8@gmail.com>
2026-03-14 16:14:07 +0530

    Add Body and RestaurantCard components; update AppLayout to include Body; enhance index.css for restaurant card styling

--- App.js
+++ App.js
@@ -61,12 +61,42 @@ const Header = () =>{
   )
 }
 
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img alt="reslogo" src="https://www.cookingcarnival.com/wp-content/uploads/2025/09/Vegetable-Dum-Biryani-5.jpg" className="res-logo"/>
+      <h3>Meghana Foods</h3>
+      <h4>South Indian, North Indian</h4>
+      <h4>4.5 stars</h4>
+    </div>
+  )
+}
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">
+       Search
+      </div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  )
+}
 
 const AppLayout = () => {
   return (
     <div>
       <Header/>
-      
+      <Body/>
     </div>
   );
 }
