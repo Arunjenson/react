78e1849c3e8d0bd329c03ca504e8a2f665135b74
Arunjenson <jensonarun8@gmail.com>
2026-03-17 21:25:11 +0530

    Refactor components: update Header and Body to use default exports; add RestaurantCard component

---
+++ src/components/RestaurantCard.jsx
@@ -0,0 +1,11 @@
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img alt="reslogo" src="https://www.cookingcarnival.com/wp-content/uploads/2025/09/Vegetable-Dum-Biryani-5.jpg" className="res-logo"/>
+      <h3>Meghana Foods</h3>
+      <h4>South Indian, North Indian</h4>
+      <h4>4.5 stars</h4>
+    </div>
+  )
+}
+export default RestaurantCard;
